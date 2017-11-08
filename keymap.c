fee35444f107e6a7aa39705e11be01dc397ac078
Wade Bowers <wade@Wade-MBP.local>
2017-11-08 11:23:01 -0500

    added escape to super + ~

--- keymap.c
+++ keymap.c
@@ -7,6 +7,7 @@ static uint16_t key_timer;
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) // this is the function signature -- just copy/paste it into your keymap file as it is.
 {
   switch(id) {
+    //this first one is untested, haven't flashed yet
     case 2:
       if (record->event.pressed) {
           breathing_speed_set(3);
@@ -89,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 [_FL] = KEYMAP_ANSI(
-  _______, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, TG(_TL), KC_INS ,  \
+  KC_ESC, KC_F1 ,KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, TG(_TL), KC_INS ,  \
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,  M(2), BL_DEC,BL_INC, BL_TOGG,KC_HOME, \
   _______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,_______,_______,        _______,KC_END, \
   _______,_______,_______,M(4),M(5),_______,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,KC_BTN1, KC_MS_U, KC_BTN2, \
